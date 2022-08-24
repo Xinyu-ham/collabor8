60308c26c59e923a48ed63878ee764777fd936a6
Xin Yu <xinyu_03@hotmail.com>
2022-08-24 14:44:12 +0800

    Added data model

--- collabor8_app/backend/models.py
+++ collabor8_app/backend/models.py
@@ -6,12 +6,6 @@ class User(models.Model):
     first_name = models.CharField(max_length=12, blank=False, null=False)
     last_name = models.CharField(max_length=20, blank=False, null=False)
 
-class Skill(models.Model):
-    name = models.CharField(max_length=20, blank=False, null=False, unique=True)
-
-class UserSkill(models.Model):
-    pass
-
 class Room(models.Model):
     name = models.CharField(max_length=24, blank=False, null=False)
     duration = models.FloatField(blank=False)
@@ -39,15 +33,52 @@ class Teammate(models.Model):
             )
 
     def __str__(self):
-        return self.room.name + ':' + self.display_name
+        return self.room.name + ': ' + self.display_name
+
+
+class Skill(models.Model):
+    name = models.CharField(max_length=20, blank=False, null=False, unique=True)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False, blank=False)
+
+
+class TeammateSkill(models.Model):
+    teammate = models.ForeignKey(Teammate, on_delete=models.CASCADE, null=False, blank=False)
+    skill = models.ForeignKey(User, on_delete=models.CASCADE, null=False, blank=False)
+
+    def validate_unique(self, *args, **kwargs) -> None:
+        super().validate_unique(*args, **kwargs)
+        if self.__class__.objects.filter(user=self.user, room=self.room):
+            raise ValidationError(
+                message=f'Team member with username {self.user.first_name} already exists in room {self.room.name}:{self.room.id}',
+                code='unique_together'
+            )
+
+    def __str__(self):
+        return self.teammate.display_name + ': ' + self.skill.name
 
 
 class Story(models.Model):
-    pass
+    title = models.CharField(max_length=50, blank=False, null=False, unique=True)
+    description = models.CharField(max_length=500, blank=False, null=False)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False, blank=False)
+
 
 class Task(models.Model):
-    pass
+    title = models.CharField(max_length=50, blank=False, null=False)
+    story = models.ForeignKey(Story, on_delete=models.CASCADE, null=False, blank=False)
+    description = models.CharField(max_length=500, blank=False, null=False)
+    estimated_hours = models.IntegerField(blank=True, null=True)
+
+
+class TaskRequirement(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=False, blank=False)
+    skill = models.ForeignKey(Skill, on_delete=models.CASCADE, null=False, blank=False)
 
 class Timeline(models.Model):
-    pass
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, null=False, blank=False)
+
+class Assignment(models.Model):
+    timeline = models.ForeignKey(Timeline, on_delete=models.CASCADE, null=False, blank=False)
+    teammate = models.ForeignKey(Teammate, on_delete=models.CASCADE, null=False, blank=False)
+    task = models.ForeignKey(Task, on_delete=models.CASCADE, null=False, blank=False)
 
